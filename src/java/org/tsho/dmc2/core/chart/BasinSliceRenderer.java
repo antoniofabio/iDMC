6f997ef62dc071bc67cfc0870125c11820a00a05
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-06-12 14:48:22 +0200

    removed unused code from basin slice renderer class

--- src/java/org/tsho/dmc2/core/chart/BasinSliceRenderer.java
+++ src/java/org/tsho/dmc2/core/chart/BasinSliceRenderer.java
@@ -50,33 +50,15 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
     /** map to be analysed */
     private SimpleMap map;
     
-    private int type;
-
-    /** parameters */
-    private double[] parameters;
-    /** now stands for the number of iterations needed to get an attractor. */
-    private int attractorLimit;
-    /** now stands for the number of iterations done for each point either to draw the attractor or to check whether converges to an attractor. */
-    private int attractorIterations;
-    /** Absolute value for which a coordinate value can be considered 'infinity' */    
-    private double infinity;
-    /** number of trials to preliminarly find attractors */
-    private int numberOfRandomPoints;
-
     // flags    
     private boolean stopped;
     private boolean bigDotsEnabled = false;
     
     // internal state
     private int state;
-    private Grid grid;
     private int gridWidth;
     private int gridHeight;
     
-    /** Records for each attractor found a point in its basin. 
-     * This way, also after the image is enlarged,
-     * we may generate the attractors and basins anew with the same colors. */
-    private Vector attractorsSamplePoints;
     private Graphics2D g2;
     private BufferedImage image;
     private int imageX, imageY, rate;
@@ -98,8 +80,7 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
     final Graphics2D g2, final Rectangle2D dataArea,
     final PlotRenderingInfo info) {
         basinComponent.setDataobject(null);
-        attractorsSamplePoints = new Vector();
-        
+
         state = STATE_RUNNING;
 
         gridWidth = (int) dataArea.getWidth();
@@ -123,31 +104,21 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
             maxCoordinate=Math.abs(rangeAxis.getLowerBound());
         if (Math.abs(rangeAxis.getUpperBound())>maxCoordinate)
             maxCoordinate=Math.abs(rangeAxis.getUpperBound());
-        if (infinity<maxCoordinate)
-            infinity=maxCoordinate+1;
         
-        grid = new Grid(
-        		new double[] {domainAxis.getLowerBound(), 
-        				domainAxis.getUpperBound(), rangeAxis.getLowerBound(),
-        				rangeAxis.getUpperBound()}, 
-        				gridHeight, gridWidth);
         imageData = ((DataBufferInt) raster.getDataBuffer()).getData();
         rate = gridHeight * gridWidth / 100;
-        attractorsSamplePoints = new Vector();
         
         //TODO: step through C level basin object
         
-        /** Now that grid is computed, pass it to the BasinComponent for possible storing */
-        int [] tmp = grid.getData();
-        grid.setData((int[]) tmp.clone());//from now on, grid data is disconnected from image data
-        basinComponent.setDataobject(grid);
+        /** TODO: pass basin data to the plot component */
+        basinComponent.setDataobject(null);
 
         drawImage();
         state = STATE_FINISHED;
     }
     
     public void drawImage() {
-    	int [] gridData = grid.getData();
+    	int [] gridData = new int[gridWidth * gridHeight];
     	gridColors = colorSettings.getArray();
     	int code;
     	for(int i=0; i<gridData.length; i++) { //color code traslation
@@ -161,6 +132,7 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
         if(bigDotsEnabled) {
         	double[] tmpAttr;
         	int [] tmpXy;
+                /* TODO: step through basins attractors
 	        for(int j=0; j<attractorsSamplePoints.size(); j++) {
 	        	g2.setColor(new Color(gridColors[Math.min(2+j*2, gridColors.length-1)]));
 	        	tmpAttr = (double[]) attractorsSamplePoints.elementAt(j);
@@ -170,22 +142,13 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
 	        		iterate(tmpAttr);
 	        	}
 	        }
+                 */
         }
     }
     
     public void initialize(
     final double[] parameters, final int attractorLimit,
     final int attractorIterations, final int trials, final double infinity) {
-        this.parameters = parameters;
-        this.attractorLimit = attractorLimit;
-        this.attractorIterations = attractorIterations;
-        this.infinity = infinity;
-        numberOfRandomPoints=trials;
-    }
-    
-    public boolean iterate(double[] p) {
-        map.evaluate(parameters, (double[]) p.clone(), p);
-        return true;
     }
     
     public LegendItemCollection getLegendItems() {
@@ -204,50 +167,9 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
         state = i;
     }
     
-    public boolean isPointInfinite(double[] p) {
-        Double a0=new Double(p[0]);
-        Double a1=new Double(p[1]);
-        if (a0.isInfinite() || a1.isInfinite() || a0.isNaN()|| a1.isNaN() || Math.abs(p[0])>infinity || Math.abs(p[1])>infinity)
-            return true;
-        else
-            return false;
-    }
-
-    public void setType(int type){
-        this.type=type;
-    }
-    
     public void setBigDotsEnabled(boolean flag){
         bigDotsEnabled=flag;
     }
-
-	/**
-	 * @return the grid
-	 */
-	public Grid getGrid() {
-		return grid;
-	}
-
-	/**
-	 * @return the infinity
-	 */
-	public double getInfinity() {
-		return infinity;
-	}
-
-	/**
-	 * @return the attractorLimit
-	 */
-	public int getAttractorLimit() {
-		return attractorLimit;
-	}
-
-	/**
-	 * @return the attractorIterations
-	 */
-	public int getAttractorIterations() {
-		return attractorIterations;
-	}
 
 	/**
 	 * @return the stopped
@@ -257,24 +179,10 @@ public class BasinSliceRenderer implements DmcPlotRenderer {
 	}
 
 	/**
-	 * @return the numberOfRandomPoints
-	 */
-	public int getNumberOfRandomPoints() {
-		return numberOfRandomPoints;
-	}
-
-	/**
 	 * @return the rate
 	 */
 	public int getRate() {
 		return rate;
-	}
-
-	/**
-	 * @return the attractorsSamplePoints
-	 */
-	public Vector getAttractorsSamplePoints() {
-		return attractorsSamplePoints;
 	}
 
 	/**
